7dbd68229e519583555b1d4c995a026df4184dae
Masanobu Uchida <manbbry0222@gmail.com>
2019-04-28 08:56:00 +0000

    layout create

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,20 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title","Home|instagram-clone"
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-    assert_select "title","Help|instagram-clone"
+    assert_select "title","instagram-clone"
   end
   
     test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title","About|instagram-clone"
   end
 
+ test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact|instagram-clone"
+  end
 end
